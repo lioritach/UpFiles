f722f15d88d64cc5ae4b87b7bed6217ce7f5cc82
Lior Itach <44543472+lioritach@users.noreply.github.com>
2020-06-13 22:27:23 +0300

    Update download.js

--- app/src/helpers/download.js
+++ app/src/helpers/download.js
@@ -4,7 +4,7 @@ import _ from 'lodash'
 
 export const getDownloadInfo = (id) => {
 
-    const url = `${apiUrl}/posts/${id}`;
+    const url = `${apiUrl}/posts/${id}`; //the url for downloadig the files
 
     return axios.get(url);
 
